962d3a872ecd8a1189862a8ed606629cef56faa7
dcist-robot <dcist>
2020-01-27 12:54:26 -0500

    Modifications for ouster

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -50,19 +50,19 @@ using namespace std;
 
 typedef pcl::PointXYZI  PointType;
 
-extern const string pointCloudTopic = "/velodyne_points";
-extern const string imuTopic = "/imu/data";
+extern const string pointCloudTopic = "/os1_cloud_node/points";
+extern const string imuTopic = "/no_imu";
 
 // Save pcd
 extern const string fileDirectory = "/tmp/";
 
 // VLP-16
-extern const int N_SCAN = 16;
-extern const int Horizon_SCAN = 1800;
-extern const float ang_res_x = 0.2;
-extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
-extern const int groundScanInd = 7;
+//extern const int N_SCAN = 16;
+//extern const int Horizon_SCAN = 1800;
+//extern const float ang_res_x = 0.2;
+//extern const float ang_res_y = 2.0;
+//extern const float ang_bottom = 15.0+0.1;
+//extern const int groundScanInd = 7;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
@@ -83,12 +83,12 @@ extern const int groundScanInd = 7;
 // extern const int groundScanInd = 7;
 
 // Ouster OS1-64
-// extern const int N_SCAN = 64;
-// extern const int Horizon_SCAN = 1024;
-// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
-// extern const float ang_res_y = 33.2/float(N_SCAN-1);
-// extern const float ang_bottom = 16.6+0.1;
-// extern const int groundScanInd = 15;
+extern const int N_SCAN = 64;
+extern const int Horizon_SCAN = 1024;
+extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+extern const float ang_res_y = 33.2/float(N_SCAN-1);
+extern const float ang_bottom = 16.6+0.1;
+extern const int groundScanInd = 15;
 
 extern const bool loopClosureEnableFlag = false;
 extern const double mappingProcessInterval = 0.3;
